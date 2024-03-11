347489cd3a9af80cf1af431c9983f65345b18972
Tu Nombre <santithevenetvalles@gmail.com>
2024-03-11 22:09:33 +0100

    First reactive component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,14 @@
 <script>
+    import NestedButton from "./NestedButton.svelte";
+	let string = `this string contains some <strong>HTML!!!</strong>`;
+
     let src = '/favicon.png';
 </script>
 
 <h1>Hello Santiago</h1>
+<p>{@html string}</p>
 <img src={src} alt="imagen chingona" />
+<NestedButton/>
 
 <style>
     h1 {
---
+++ src/routes/NestedButton.svelte
@@ -0,0 +1,15 @@
+<script>
+    let count = 0;
+
+    function increment() {
+        count += 1
+    }
+</script>
+
+<button on:click={increment}>Clicked {count} {count === 1 ? "time" : "times"}</button>
+
+<style>
+    button {
+        background-color: aqua;
+    }
+</style>
