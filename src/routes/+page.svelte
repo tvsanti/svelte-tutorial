44eeff429008ffed14ddeec634d0692df721060d
Tu Nombre <santithevenetvalles@gmail.com>
2024-03-11 21:50:53 +0100

    Basic structure svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let src = '/favicon.png';
+</script>
+
+<h1>Hello Santiago</h1>
+<img src={src} alt="imagen chingona" />
+
+<style>
+    h1 {
+        color: red;
+        background-color: blue;
+    }
+</style>
