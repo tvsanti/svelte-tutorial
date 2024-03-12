c66da3b307393b04fd12c198f64802e2f2e16cdf
Tu Nombre <santithevenetvalles@gmail.com>
2024-03-12 21:23:32 +0100

    More reactivity

--- src/routes/NestedButton.svelte
+++ src/routes/NestedButton.svelte
@@ -1,13 +1,17 @@
 <script>
     let count = 0;
+    $: doubled = count * 2
 
+    $: if (count >= 5) {
+        alert(2)
+    }
     function increment() {
         count += 1
     }
 </script>
 
 <button on:click={increment}>Clicked {count} {count === 1 ? "time" : "times"}</button>
-
+<p>{count} doubled is {doubled}</p>
 <style>
     button {
         background-color: aqua;
