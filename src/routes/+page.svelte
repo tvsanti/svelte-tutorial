a1f10848b3dbd848d514a711012a4dc1c8e21622
Tu Nombre <santithevenetvalles@gmail.com>
2024-03-13 22:08:27 +0100

    Events

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,9 @@
 <script>
-	import { getRandomNumber } from './utils.js';
+	import Outer from './Outer.svelte';
 
-	let promise = getRandomNumber();
-
-	function handleClick() {
-		promise = getRandomNumber();
+	function handleMessage(event) {
+		alert(event.detail.text);
 	}
 </script>
 
-<button on:click={handleClick}>
-	generate random number
-</button>
-
-{#await promise}
-	<p>...waiting</p>
-{:then number}
-	<p>The number is {number}</p>
-{:catch error}
-	<p style="color: red">{error.message}</p>
-{/await}
+<Outer on:message={handleMessage} />
