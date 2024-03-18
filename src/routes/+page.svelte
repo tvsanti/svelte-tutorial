f591ee835756049b79722f869b92892c8d80dd43
Tu Nombre <santithevenetvalles@gmail.com>
2024-03-18 22:07:50 +0100

    Readble stores

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,15 @@
-<script lang="ts">
-	import { count } from './stores.js';
-	import Incrementer from './Incrementer.svelte';
-	import Decrementer from './Decrementer.svelte';
-	import Resetter from './Resetter.svelte';
-  import { onDestroy } from 'svelte';
+<script>
+	import { time } from './stores.js';
 
-	// let count_value: number;
-
-	// const unsubscribe = count.subscribe((value) => {
-	// 	console.log("Update")
-	// 	count_value = value;
-	// });
-
-	// onDestroy(unsubscribe);
+	const formatter = new Intl.DateTimeFormat(
+		'en',
+		{
+			hour12: false,
+			hour: '2-digit',
+			minute: '2-digit',
+			second: '2-digit'
+		}
+	);
 </script>
 
-<h1>The count is {$count}</h1>
-
-<Incrementer />
-<Decrementer />
-<Resetter />
+<h1>The time is {formatter.format($time)}</h1>
